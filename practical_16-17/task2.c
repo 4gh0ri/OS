50cbc4b492b3a5544b56b25148385401d277ea4a
4gh0ri <82905483+4gh0ri@users.noreply.github.com>
2021-04-29 22:50:06 +0530

    Update task2.c

--- practical_16-17/task2.c
+++ practical_16-17/task2.c
@@ -5,29 +5,29 @@
 #include <semaphore.h>
 #include <stdlib.h>
 
-sem_t sem;//Declaring Semaphore
+sem_t sem; //Declaring Semaphore
 int c;
-char str[1000],ch;
+char str[1000], ch;
 FILE *fp;
 //Function For appending
-void* append()
+void *append()
 {
-	sem_wait(&sem);
-	c++;
-	fp = fopen("/home/photon/Christ/practical_16-17/bca.txt","a+");//Opening file in append mode
-	printf("Thread %d is entering...\n",c);
-	printf("Enter Sentence:");
-	fgets(str,sizeof(str),stdin);
-	fputs(str,fp);
-	fclose(fp);
-	printf("Thread %d is exiting...\n\n",c);
-	sem_post(&sem);
+    sem_wait(&sem);
+    c++;
+    fp = fopen("/home/photon/Christ/practical_16-17/bca.txt", "a+"); //Opening file in append mode
+    printf("Thread %d is entering...\n", c);
+    printf("Enter Sentence:");
+    fgets(str, sizeof(str), stdin);
+    fputs(str, fp);
+    fclose(fp);
+    printf("Thread %d is exiting...\n\n", c);
+    sem_post(&sem);
 }
 //Function for printing data from file
-void readFile(FILE * fp)
+void readFile(FILE *fp)
 {
     char ch;
-    do 
+    do
     {
         ch = fgetc(fp);
         putchar(ch);
@@ -36,16 +36,16 @@ void readFile(FILE * fp)
 }
 int main()
 {
-	sem_init(&sem,0,1);//Intialising semaphore
-	pthread_t t1,t2,t3;
-	pthread_create(&t1,NULL,append,NULL);
-	pthread_create(&t2,NULL,append,NULL);
-	pthread_create(&t3,NULL,append,NULL);
-	pthread_join(t1,NULL);
-    pthread_join(t2,NULL);
-    pthread_join(t3,NULL);
+    sem_init(&sem, 0, 1); //Intialising semaphore
+    pthread_t t1, t2, t3;
+    pthread_create(&t1, NULL, append, NULL);
+    pthread_create(&t2, NULL, append, NULL);
+    pthread_create(&t3, NULL, append, NULL);
+    pthread_join(t1, NULL);
+    pthread_join(t2, NULL);
+    pthread_join(t3, NULL);
     printf("\n---Contents of File---\n");
-    fp = fopen("/home/photon/Christ/practical_16-17/bca.txt","rt");
+    fp = fopen("/home/photon/Christ/practical_16-17/bca.txt", "rt");
     if (fp == NULL)
     {
         printf("Cannot open file \n");
@@ -53,5 +53,5 @@ int main()
     readFile(fp);
     fclose(fp);
     sem_destroy(&sem);
-	return 0;
+    return 0;
 }
